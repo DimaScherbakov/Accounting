a1c06757e0a691dc446eb975515834c50ae8891d
dmytro <dima.r.scherbakov@gmail.com>
2018-02-20 16:50:06 +0200

    weak solution about refresh table

--- Accountig/Form1.cs
+++ Accountig/Form1.cs
@@ -14,6 +14,45 @@ namespace Accountig
     public partial class MainWindow : Form
     {
         SqlConnection SqlConnection;
+        async void Refresh()
+        {
+            SqlConnection SqlConnection;
+            string connectionString = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = 'C:\Users\dmytro\Documents\Visual Studio 2015\Projects\Accountig\Accountig\Database.mdf'; Integrated Security = True";
+            // открытие асинхронного запроса
+            SqlConnection = new SqlConnection(connectionString);
+            await SqlConnection.OpenAsync();
+            // поучить данные в табл представлении
+            SqlDataReader SqlReader = null;
+            //запрос sqlCommand(инструкция, )
+            SqlCommand command = new SqlCommand("SELECT * FROM [Invoice] ", SqlConnection);
+
+            // обработчик
+            try
+            {
+                // присвоить результат выполнения команды для дальнейшего вывода
+                SqlReader = await command.ExecuteReaderAsync();
+                while (await SqlReader.ReadAsync())
+                {
+                    // вывод таблицы
+                    listBox1.Items.Add(Convert.ToString(SqlReader["Id"]) + "  " + Convert.ToString(SqlReader["Name"]) + "  " + Convert.ToString(SqlReader["Date"]) + "  " + Convert.ToString(SqlReader["Account"]));
+                }
+            }
+            catch (Exception ex)
+            {
+                // показать сообщение MessageBox.Show(текст, заголовок, кнопка, иконка)
+                MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                // обязательно закрыть ридер если ему все же было присвоено значение
+                if (SqlReader != null)
+                {
+                    SqlReader.Close();
+                }
+            }
+
+        }
+
 
         public MainWindow()
         {
@@ -84,34 +123,7 @@ namespace Accountig
                 await command.ExecuteNonQueryAsync();
 
                 listBox1.Items.Clear();
-                SqlDataReader SqlReader = null;
-                //запрос sqlCommand(инструкция, )
-                command = new SqlCommand("SELECT * FROM [Invoice] ", SqlConnection);
-
-                // обработчик
-                try
-                {
-                    // присвоить результат выполнения команды для дальнейшего вывода
-                    SqlReader = await command.ExecuteReaderAsync();
-                    while (await SqlReader.ReadAsync())
-                    {
-                        // вывод таблицы
-                        listBox1.Items.Add(Convert.ToString(SqlReader["Id"]) + "  " + Convert.ToString(SqlReader["Name"]) + "  " + Convert.ToString(SqlReader["Date"]) + "  " + Convert.ToString(SqlReader["Account"]));
-                    }
-                }
-                catch (Exception ex)
-                {
-                    // показать сообщение MessageBox.Show(текст, заголовок, кнопка, иконка)
-                    MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-                finally
-                {
-                    // обязательно закрыть ридер если ему все же было присвоено значение
-                    if (SqlReader != null)
-                    {
-                        SqlReader.Close();
-                    }
-                }
+                Refresh();
             }
             else
             {
