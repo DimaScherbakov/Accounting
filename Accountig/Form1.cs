4f2c93cba8e9ae6f8b70b8a638ee6b9a88130134
dmytro <dima.r.scherbakov@gmail.com>
2018-02-20 13:37:15 +0200

    created database show table with clients

--- Accountig/Form1.cs
+++ Accountig/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ namespace Accountig
 {
     public partial class MainWindow : Form
     {
+        SqlConnection SqlConnection;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -22,9 +25,42 @@ namespace Accountig
 
         }
 
-        private void MainWindow_Load(object sender, EventArgs e)
+        private async void MainWindow_Load(object sender, EventArgs e)
         {
+            // подключение к БД
+            string connectionString = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = 'C:\Users\dmytro\Documents\Visual Studio 2015\Projects\Accountig\Accountig\Database.mdf'; Integrated Security = True";
+            // открытие асинхронного запроса
+            SqlConnection = new SqlConnection(connectionString);
+            await SqlConnection.OpenAsync();
+            // поучить данные в табл представлении
+            SqlDataReader SqlReader = null;
+            //запрос sqlCommand(инструкция, )
+            SqlCommand command = new SqlCommand("SELECT * FROM [Invoice] ", SqlConnection);
 
+            // обработчик
+            try
+            {
+                // присвоить результат выполнения команды для дальнейшего вывода
+                SqlReader = await command.ExecuteReaderAsync();
+                while (await SqlReader.ReadAsync())
+                {
+                    // вывод таблицы
+                    listBox1.Items.Add(Convert.ToString(SqlReader["Id"]) + "  " + Convert.ToString(SqlReader["Name"]) + "  " + Convert.ToString(SqlReader["Date"]) + "  " + Convert.ToString(SqlReader["Account"]));
+                }
+            }
+            catch (Exception ex)
+            {
+                // показать сообщение MessageBox.Show(текст, заголовок, кнопка, иконка)
+                MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error)
+            }
+            finally
+            {
+                // обязательно закрыть ридер если ему все же было присвоено значение
+                if (SqlReader != null)
+                {
+                    SqlReader.Close();
+                }        
+            }
         }
     }
 }
